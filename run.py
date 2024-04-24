3cb3695277222d4fbad4112ecacc9f7c27747888
zyadelmahdy <35871175+zyadelmahdy@users.noreply.github.com>
2024-04-24 02:22:39 +0200

    added the whole app to tasks.py
    added the whole app to tasks.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-from shared.latimes import LATimes
+# from shared.latimes import LATimes
 
-with LATimes(teardown=True) as bot:
-    bot.load_first_page()
-    print('Successfully loaded LA Times first page.')
-    bot.search('education')
-    bot.filter([])
-    bot.sort_newest()
-    bot.pull_data()
-    # bot.export() 
+# with LATimes(teardown=True) as bot:
+#     bot.load_first_page()
+#     print('Successfully loaded LA Times first page.')
+#     bot.search('education')
+#     bot.filter([])
+#     bot.sort_newest()
+#     bot.pull_data()
+#     # bot.export() 
